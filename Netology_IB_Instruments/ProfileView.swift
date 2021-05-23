6a5860dc8f3da2dbce655d15bd13073231af6062
cgerasimovitch <c.gerasimovitch@gmail.com>
2021-05-23 23:18:25 +0300

    Add project

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  
+//
+//  Created by Admin on 5/17/21.
+//
+
+import UIKit
+
+class ProfileView: UIView
+{
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+}
